80f4eb31b55add3c9172e0f1479a2232b05a7edf
timhealey3 <timhealey34@gmail.com>
2024-06-11 00:23:24 -0400

    First push

--- App.tsx
+++ App.tsx
@@ -1,95 +1,180 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface Job {
+  name: string;
+  pay: number;
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+interface AllJobs {
+  blacksmith: Boolean;  // Dwarve
+  bowmaker: Boolean;    // elf
+  innkeeper: Boolean;   // hobbit
+  farmer: Boolean;      // human
+  miner: Boolean;       // Dwarve
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+interface Parents {
+  motherAge: number;
+  fatherAge: number;
+  money: number;
+  job: Job;
+}
+
+interface Person {
+  age: number;
+  city: string;
+  job: Job;
+  money: number;
+  name: string;
+  parents: Parents;
+  possibleJobs: AllJobs;
+  race: String;
+  royalty: Boolean;
+  year: number;
+}
+
+const initParents = (): Parents => {
+  return {
+    motherAge: 30,
+    fatherAge: 32,
+    money: 200,
+    job: parentJob(),
+  };
+};
+
+const parentJob = (): Job => {
+  return {
+    name: 'Random Job',
+    pay: 5,
+  };
+};
+
+const initJob = (): AllJobs => {
+  return {
+    blacksmith: false,
+    bowmaker: false,
+    innkeeper: false,
+    farmer: false,
+    miner: false,
+  };
+};
+
+const initPleb = (): AllJobs => {
+  return {
+    blacksmith: true,
+    bowmaker: true,
+    innkeeper: false,
+    farmer: true,
+    farmer: true,
+    miner: true,
+  };
+};
+
+const generateJob = (): Job => {
+  return {
+    name: 'None',
+    pay: 0,
+  };
+};
+
+const GeneratePerson = (): Person => {
+  return {
+    age: 0,
+    city: 'Hobbiton',
+    job: generateJob(),
+    money: 0,
+    name: 'Frodo Baggins',
+    parents: initParents(),
+    possibleJobs: initJob(),
+    race: 'Dwarve',
+    royalty: false,
+    year: Math.floor(Math.random() * 300 + 27),
+  };
+};
+
+function App(): React.JSX.Element {
+  const [person, setPerson] = React.useState<Person | null>(null);
+
+  const handleGeneratePerson = () => {
+    const newPerson = GeneratePerson();
+    setPerson(newPerson);
+  };
+
+  const helpOutParent = () => {
+    // TODO make it a choice
+    person.job.name = 'Helping';
+    person.job.pay = 2;
+  };
+
+  const increaseAge = () => {
+    setPerson({
+      ...person,
+      age: person.age + 1,
+      year: person?.year + 1,
+      money: person?.money + person.job.pay,
+      parents.money: person?.parents.job.pay + person?.money,
+    });
+    jobAge();
+  };
+
+  const jobAge = () => {
+    const probability = Math.floor(Math.random() * 101);
+    // help your dad out 8 - 17
+    if (person.age < 17 && person.age > 8 && person?.royalty == false) {
+      if (probability < 5 && person.job.name === 'None') {
+        helpOutParent();
+      }
+    }
+    // at age 18 make them able to get full time plebian jobs
+    if (person.age === 18) {
+      initPleb();
+    }
+    // if rome is at war have this pop up
+    if (person.age >= 100) {
+      signUpSoldier();
+    }
+    // 17 <= jobs
+    // farmer, merchant, soldier, gladiator, engineer, builder, fisherman, pottery
+    // accountant, tax collector, senator, government official
+  };
+
+  const signUpSoldier = () => {
+    console.log('Signed Up');
+    //setPerson({ ...person, possibleJobs.soldier: true});
+    //person?.possibleJobs.soldier = true;
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <StatusBar />
+      <ScrollView>
+        <View>
+          <Button title="Generate Person" onPress={handleGeneratePerson} />
+          {person && (
+            <View>
+              <Text>Name: {person.name}</Text>
+              <Text>Age: {person.age}</Text>
+              <Text>
+                Job: {person.job.name} Pay: {person.job.pay}
+              </Text>
+              <Text>Bank: {person.money}</Text>
+              <Text>Dad age: {person.parents.fatherAge}</Text>
+              <Text>Mom age: {person.parents.motherAge}</Text>
+              <Text>Parent Money: {person.parents.money}</Text>
+              <Text>City: {person.city}</Text>
+              <Text>Year: {person.year}</Text>
+            </View>
+          )}
+          <Button title="Age" onPress={increaseAge} />
         </View>
       </ScrollView>
     </SafeAreaView>
